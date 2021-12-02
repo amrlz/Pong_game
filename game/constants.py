5aa6fd20f67257e56e00db84c9ea356a83ee36e3
abednell <abednell@imaginarycloud.com>
2021-12-02 16:39:07 +0000

    first commit, this is a pong game project

---
+++ game/constants.py
@@ -0,0 +1,10 @@
+class Constants:
+    screen_height = 600
+    screen_width= 800
+    starting_lives = 3
+    paddle_speed = 4
+    collision_thershold = 12
+    max_ball_speed = 10
+    brick_start = 16
+    brick_cols = 12
+    brick_rows = 6
